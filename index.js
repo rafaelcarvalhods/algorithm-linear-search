8afa9a00b03edd6c3370326b0e649518e7806648
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-02-20 17:11:26 -0400

    Practing

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-let listNames = ["Rafael", "Marcia", "Ester"]
+// Step 1: First, read the search element (Target element) in the array.
+// Step 2: In the second step compare the search element with the first element in the array.
+// Step 3: If both are matched, display “Target element is found” and terminate the Linear Search 
+// function.
+// Step 4: If both are not matched, compare the search element with the next element in the array.
+// Step 5: In this step, repeat steps 3 and 4 until the search (Target) element is compared with the 
+// last element of the array.
+// Step 6: If the last element in the list does not match, the Linear Search Function will be 
+// terminated, and the message “Element is not found” will be displayed.
 
-function searchName(name){
-    for(let i=0; i<listNames.length; i++){
-        if(name == listNames[i]){
+function linearSearch(arr, name) {
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] == name){
             return i
         }
     }
@@ -10,10 +18,13 @@ function searchName(name){
     return -1
 }
 
-result = searchName("Ester")
+let list = ["Ester", "Rafael", "Marcia"]
+let fName = "Josue"
 
-if(result == -1){
-    console.log("The name is not in this list.")
+result = linearSearch(list, fName)
+
+if (result == -1){
+    console.log(`The name ${fName} is not on the list`)
 } else {
-    console.log(`The element is in the index ${result}.`)
+    console.log(`The name ${fName} is on the index ${result}.`)
 }
